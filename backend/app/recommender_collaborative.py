9babbea17bc17c41a23aeb6b93d250fdb3dba8c4
Sanjay <sanjay4subbu@gmail.com>
2026-05-30 12:28:01 +0530

    commented collaborative

--- backend/app/recommender_collaborative.py
+++ backend/app/recommender_collaborative.py
@@ -1,194 +1,194 @@
-import pickle
-import pandas as pd
-from app.tmdb import fetch_tmdb_poster
+# import pickle
+# import pandas as pd
+# from app.tmdb import fetch_tmdb_poster
 
-movies = pickle.load(
-    open(
-        'app/artifacts/collab_based/collaborative_movies.pkl',
-        'rb'
-    )
-)
+# movies = pickle.load(
+#     open(
+#         'app/artifacts/collab_based/collaborative_movies.pkl',
+#         'rb'
+#     )
+# )
 
-model = pickle.load(
-    open(
-        'app/artifacts/collab_based/collaborative_model.pkl',
-        'rb'
-    )
-)
+# model = pickle.load(
+#     open(
+#         'app/artifacts/collab_based/collaborative_model.pkl',
+#         'rb'
+#     )
+# )
 
-sparse_matrix = pickle.load(
-    open(
-        'app/artifacts/collab_based/collaborative_sparse.pkl',
-        'rb'
-    )
-)
+# sparse_matrix = pickle.load(
+#     open(
+#         'app/artifacts/collab_based/collaborative_sparse.pkl',
+#         'rb'
+#     )
+# )
 
-movie_ids = pickle.load(
-    open(
-        "app/artifacts/collab_based/movie_ids.pkl",
-        "rb"
-    )
-)
-movie_id_to_index = pickle.load(
-    open(
-        'app/artifacts/collab_based/movie_id_to_index.pkl',
-        'rb'
-    )
-)
+# movie_ids = pickle.load(
+#     open(
+#         "app/artifacts/collab_based/movie_ids.pkl",
+#         "rb"
+#     )
+# )
+# movie_id_to_index = pickle.load(
+#     open(
+#         'app/artifacts/collab_based/movie_id_to_index.pkl',
+#         'rb'
+#     )
+# )
 
-def recommend_collaborative(movie_name):
+# def recommend_collaborative(movie_name):
 
-    matches = movies[
-        movies['title']
-        .fillna('')
-        .str.lower()
-        .str.contains(
-            movie_name.lower()
-        )
-    ]
+#     matches = movies[
+#         movies['title']
+#         .fillna('')
+#         .str.lower()
+#         .str.contains(
+#             movie_name.lower()
+#         )
+#     ]
 
-    if matches.empty:
+#     if matches.empty:
 
-        print("Movie not found")
+#         print("Movie not found")
 
-        return []
+#         return []
 
-    movie_id = matches.iloc[0]['id']
+#     movie_id = matches.iloc[0]['id']
 
-    print("TMDB Movie ID:", movie_id)
+#     print("TMDB Movie ID:", movie_id)
 
-    if movie_id not in movie_id_to_index:
+#     if movie_id not in movie_id_to_index:
 
-        print("Movie not present in collaborative matrix")
+#         print("Movie not present in collaborative matrix")
 
-        return []
+#         return []
 
-    movie_index = movie_id_to_index[
-        movie_id
-    ]
+#     movie_index = movie_id_to_index[
+#         movie_id
+#     ]
 
-    distances, indices = model.kneighbors(
-        sparse_matrix[movie_index],
-        n_neighbors=11
-    )
+#     distances, indices = model.kneighbors(
+#         sparse_matrix[movie_index],
+#         n_neighbors=11
+#     )
 
-    recommendations = []
+#     recommendations = []
 
-    for i in range(
-        1,
-        len(indices[0])
-    ):
+#     for i in range(
+#         1,
+#         len(indices[0])
+#     ):
 
-        similar_movie_index = indices[0][i]
+#         similar_movie_index = indices[0][i]
 
-        similar_movie_id = (
-            movie_ids[
-                similar_movie_index
-            ]
-        )
+#         similar_movie_id = (
+#             movie_ids[
+#                 similar_movie_index
+#             ]
+#         )
 
-        movie_data = movies[
-            movies['id']
-            == similar_movie_id
-        ]
+#         movie_data = movies[
+#             movies['id']
+#             == similar_movie_id
+#         ]
 
-        if movie_data.empty:
-            continue
+#         if movie_data.empty:
+#             continue
 
-        movie_data = movie_data.iloc[0]
+#         movie_data = movie_data.iloc[0]
 
-        #Poster
+#         #Poster
 
-        tmdb_data = fetch_tmdb_poster(
-            movie_data.title
-        )
+#         tmdb_data = fetch_tmdb_poster(
+#             movie_data.title
+#         )
 
-        poster_url = None
+#         poster_url = None
 
-        #live TMDB poster
-        if (
-            tmdb_data
-            and tmdb_data.get("poster")
-        ):
+#         #live TMDB poster
+#         if (
+#             tmdb_data
+#             and tmdb_data.get("poster")
+#         ):
 
-            poster_url = tmdb_data["poster"]
+#             poster_url = tmdb_data["poster"]
 
-        #local dataset poster
-        elif (
-            pd.notna(
-                movie_data['poster_path']
-            )
-            and str(
-                movie_data['poster_path']
-            ).startswith("/")
-        ):
+#         #local dataset poster
+#         elif (
+#             pd.notna(
+#                 movie_data['poster_path']
+#             )
+#             and str(
+#                 movie_data['poster_path']
+#             ).startswith("/")
+#         ):
 
-            poster_url = (
-                "https://image.tmdb.org/t/p/w500"
-                + movie_data[
-                    'poster_path'
-                ]
-            )
+#             poster_url = (
+#                 "https://image.tmdb.org/t/p/w500"
+#                 + movie_data[
+#                     'poster_path'
+#                 ]
+#             )
 
-        recommendations.append({
+#         recommendations.append({
 
-            "id":
-                int(movie_data['id']),
+#             "id":
+#                 int(movie_data['id']),
 
-            "title":
-                movie_data['title'],
+#             "title":
+#                 movie_data['title'],
 
-            "overview":
-                movie_data['overview'],
+#             "overview":
+#                 movie_data['overview'],
 
-            "poster":
-                poster_url,
+#             "poster":
+#                 poster_url,
 
-            "rating":
-                (
-                    round(
-                        float(
-                            movie_data[
-                                'vote_average'
-                            ]
-                        ),
-                        1
-                    )
-                    if pd.notna(
-                        movie_data[
-                            'vote_average'
-                        ]
-                    )
-                    else None
-                ),
+#             "rating":
+#                 (
+#                     round(
+#                         float(
+#                             movie_data[
+#                                 'vote_average'
+#                             ]
+#                         ),
+#                         1
+#                     )
+#                     if pd.notna(
+#                         movie_data[
+#                             'vote_average'
+#                         ]
+#                     )
+#                     else None
+#                 ),
 
-            "release_date":
-                movie_data[
-                    'release_date'
-                ],
+#             "release_date":
+#                 movie_data[
+#                     'release_date'
+#                 ],
 
-            "runtime":
-                (
-                    int(
-                        movie_data[
-                            'runtime'
-                        ]
-                    )
-                    if pd.notna(
-                        movie_data[
-                            'runtime'
-                        ]
-                    )
-                    else None
-                ),
+#             "runtime":
+#                 (
+#                     int(
+#                         movie_data[
+#                             'runtime'
+#                         ]
+#                     )
+#                     if pd.notna(
+#                         movie_data[
+#                             'runtime'
+#                         ]
+#                     )
+#                     else None
+#                 ),
 
-            "distance":
-                round(
-                    float(
-                        distances[0][i]
-                    ),
-                    3
-                )
-        })
+#             "distance":
+#                 round(
+#                     float(
+#                         distances[0][i]
+#                     ),
+#                     3
+#                 )
+#         })
 
-    return recommendations
+#     return recommendations
